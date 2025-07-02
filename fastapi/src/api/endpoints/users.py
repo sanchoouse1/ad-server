cff404ffbb23b5b3da54a63fbbe7f28f807c5c4c
sanchoouse <sanchoouse@mail.ru>
2025-07-03 01:56:29 +0700

    реализовать вход в систему

--- fastapi/src/api/endpoints/users.py
+++ fastapi/src/api/endpoints/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Response
+from fastapi import APIRouter, Depends, HTTPException, Request, Response
 from sqlalchemy import select
 from sqlalchemy_db.db import get_async_session
 from sqlalchemy_db.models import User
@@ -34,8 +34,23 @@ async def register_user(
 
 @router.post("/login", summary="Вход в систему")
 async def login_user(
+    request: Request,
     response: Response,
     data: UserFormData,
     session: AsyncSession = Depends(get_async_session)
-):
-    pass
+) -> str:
+    # TODO: убрать после JWT
+    user_id = request.cookies.get('user_id')
+    if not user_id:
+        raise HTTPException(status_code=403, detail="Вход запрещен. Проверьте корректность полей или зарегистрируйте новый аккаунт")
+    
+    result = await session.execute(select(User).where(User.id == data.user_id))
+    existing_user = result.scalar_one_or_none()
+    if existing_user:
+        if (existing_user.hashed_password == data.hashed_password and
+            existing_user.email == data.email):
+            # TODO: Временное решение, убрать после JWT
+            response.set_cookie(key="user_id", value=existing_user.id)
+            return existing_user.id
+        else:
+            raise HTTPException(status_code=400, detail="Неверно введён логин или пароль")
