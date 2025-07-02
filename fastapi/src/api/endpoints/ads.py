0796932ff95b337993858a9d64a9b5b2a80a50b0
sanchoouse <sanchoouse@mail.ru>
2025-07-03 00:24:08 +0700

    Добавить GET эндпоинты для работы с объявлениями, обновить схемы

--- fastapi/src/api/endpoints/ads.py
+++ fastapi/src/api/endpoints/ads.py
@@ -1,3 +1,36 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
+from sqlalchemy_db.db import get_async_session
+from sqlalchemy_db.models import Ad
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.schemas import AdOut, AdDetailOut
 
 router = APIRouter()
+
+@router.get("/", summary="Просмотр списка объявлений")
+async def get_list_ads(session: AsyncSession = Depends(get_async_session)) -> list[AdOut]:
+    result = await session.execute(select(Ad))
+    return result.scalars().all()
+
+
+@router.get("/{ad_id}", summary="Обычный просмотр одного объявления")
+async def get_ad_detail(
+    ad_id: str,
+    session: AsyncSession = Depends(get_async_session)
+    ) -> AdOut:
+    ad = await session.get(Ad, ad_id)
+    if not ad:
+        raise HTTPException(404, detail="Объявление не найдено")
+    return ad
+
+
+@router.get("/{ad_id}/detail", summary="Детальный просмотр одного объявления")
+async def get_ad_detail(
+    ad_id: str,
+    session: AsyncSession = Depends(get_async_session)
+    ) -> AdDetailOut:
+    ad = await session.get(Ad, ad_id)
+    if not ad:
+        raise HTTPException(404, detail="Объявление не найдено")
+    await session.refresh(ad, attribute_names=["owner", "comments"])
+    return ad
--- fastapi/src/schemas.py
+++ fastapi/src/schemas.py
@@ -1,4 +1,31 @@
 from pydantic import BaseModel
+from uuid import uuid4
+from sqlalchemy_db.models import AdType, UserType
 
-class UserCreate(BaseModel):
-    pass
+class CommentOut(BaseModel):
+    id: str
+    text: str
+    author_id: str
+    ad_id: str
+
+
+class UserInfo(BaseModel):
+    id: str
+    email: str
+    role: UserType
+
+
+class AdOut(BaseModel):
+    id: str
+    title: str
+    description: str
+    type: AdType
+    owner_id: str
+
+class AdDetailOut(BaseModel):
+    id: str
+    title: str
+    description: str
+    type: AdType
+    owner: UserInfo
+    comments: list[CommentOut]
